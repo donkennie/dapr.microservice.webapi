954558f846f57542f862e406bc77e0f3c939071d
donkennie <ajeigbekehinde160@gmail.com>
2025-05-13 16:18:55 +0100

    doing something

--- dapr.microservice.webapi2/Program.cs
+++ dapr.microservice.webapi2/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers().AddDapr();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,6 +21,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
 app.MapSubscribeHandler();
 
 app.Run();
